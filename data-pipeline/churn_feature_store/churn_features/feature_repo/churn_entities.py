cd97c13e63f8e4ffcae1cf87c18cd031554bc946
BinhAnndapoet <dinhlebinhan2005@gmail.com>
2026-06-16 17:55:04 +0700

    change value type from int64 to string

--- data-pipeline/churn_feature_store/churn_features/feature_repo/churn_entities.py
+++ data-pipeline/churn_feature_store/churn_features/feature_repo/churn_entities.py
@@ -5,7 +5,7 @@ customer = Entity(
     name="customer_id",
     description="A customer entity with unique customer ID",
     join_keys=["customer_id"],
-    value_type=ValueType.INT64,
+    value_type=ValueType.STRING,
     tags = {
         "owner": "data__team",
         "domain": "customer_analytics",
